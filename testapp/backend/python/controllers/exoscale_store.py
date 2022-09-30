5a81244a1e193da1eef29811bb5c979864c74a97
antoine.delabays <antoine.delabays@edu.hefr.ch>
2022-09-30 20:20:42 +0200

    add changes

--- testapp/backend/python/controllers/exoscale_store.py
+++ testapp/backend/python/controllers/exoscale_store.py
@@ -15,13 +15,12 @@ class ExoStore(Store):
         file_name = '/tmp/'+ item.id+'.json'
 
         storage_client = exoscale.Exoscale()
-        storage_client.storage.create_bucket(self.bucket_name, zone="ch-gva-2")
         bucket = storage_client.storage.get_bucket(self.bucket_name)
         
         f = open(file_name, "w")
         f.write(json.dumps(item.__dict__))
         f.close()
-        bucket.put_file(file_name)    
+        bucket.put_file(file_name, file_name)    
     
     def getAllItems(self):
         storage_client = exoscale.Exoscale()
