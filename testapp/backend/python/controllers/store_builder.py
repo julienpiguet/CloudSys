88a60e0348b89aea4a6f98da8959a19e0fecdb9a
DoSomeWaffles <denis.rosset@bluewin.ch>
2022-10-02 22:18:33 +0200

    Update store_builder.py

--- testapp/backend/python/controllers/store_builder.py
+++ testapp/backend/python/controllers/store_builder.py
@@ -2,12 +2,15 @@ import imp
 from .local_store import LocalStore
 from .google_store import GoogleStore
 from .exoscale_store import ExoStore
+from .aws_store import AWSStore
 
 def get_store(name='local', arg = None):
     if name == 'google':
         return GoogleStore(arg) if arg != None else GoogleStore()
-    if name == 'exoscale':
-        return ExoStore(arg) if arg != None else ExoStore()    
+    elif name == 'exoscale':
+        return ExoStore(arg) if arg != None else ExoStore()  
+    elif name == 'aws':
+        return AWSStore(arg) if arg != None else AWSStore()
     else:
         return LocalStore(arg) if arg != None else LocalStore()
     
