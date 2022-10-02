11393643583c60568a99dafcab3abe27dde27204
Julien Piguet <13250437+julienpiguet@users.noreply.github.com>
2022-10-02 13:24:20 +0200

    update path

--- testapp/backend/python/gunicorn_conf.py
+++ testapp/backend/python/gunicorn_conf.py
@@ -1,7 +1,7 @@
 from multiprocessing import cpu_count
 
 # Socket Path
-bind = 'unix:/home/julpiguet/CloudSys/testapp/backend/python/gunicorn.sock'
+bind = 'unix:/var/www/CloudSys/testapp/backend/python/gunicorn.sock'
 
 # Worker Options
 workers = cpu_count() + 1
@@ -9,5 +9,5 @@ worker_class = 'uvicorn.workers.UvicornWorker'
 
 # Logging Options
 loglevel = 'debug'
-accesslog = '/home/julpiguet/CloudSys/testapp/backend/python/access_log'
-errorlog =  '/home/julpiguet/CloudSys/testapp/backend/python/error_log'
+accesslog = '/var/www/CloudSys/testapp/backend/python/access_log'
+errorlog =  '/var/www/CloudSys/testapp/backend/python/error_log'
