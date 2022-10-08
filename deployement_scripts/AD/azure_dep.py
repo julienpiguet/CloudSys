29694e32538f6f487cbedb63bf927c7c4e196ad3
Antoine Delabays <antoine@XPS-ANTOINE.localdomain>
2022-10-08 17:08:33 +0200

    add script

--- deployement_scripts/AD/azure_dep.py
+++ deployement_scripts/AD/azure_dep.py
@@ -21,8 +21,8 @@ resource_client = ResourceManagementClient(credential, subscription_id)
 
 # Constants we need in multiple places: the resource group name and the region
 # in which we provision resources. You can change these values however you want.
-RESOURCE_GROUP_NAME = "PythonAzureExample-VM-rg"
-LOCATION = "westus2"
+RESOURCE_GROUP_NAME = "TP1-Azure-VM-GRP3"
+LOCATION = "westeurope"
 
 # Provision the resource group.
 rg_result = resource_client.resource_groups.create_or_update(RESOURCE_GROUP_NAME,
@@ -38,6 +38,9 @@ print(f"Provisioned resource group {rg_result.name} in the {rg_result.location} 
 # at https://docs.microsoft.com/azure/developer/python/azure-sdk-example-resource-group
 
 
+
+########################################       VM1 - frontend   #########################
+
 # Step 2: provision a virtual network
 
 # A virtual machine requires a network interface client (NIC). A NIC requires
@@ -46,11 +49,11 @@ print(f"Provisioned resource group {rg_result.name} in the {rg_result.location} 
 # can provision the VM.
 
 # Network and IP address names
-VNET_NAME = "python-example-vnet"
-SUBNET_NAME = "python-example-subnet"
-IP_NAME = "python-example-ip"
-IP_CONFIG_NAME = "python-example-ip-config"
-NIC_NAME = "python-example-nic"
+VNET_NAME = "TP1_GRP3-frt"
+SUBNET_NAME = "TP1_GRP3-frt-subnet"
+IP_NAME = "TP1_GRP3-frt-ip"
+IP_CONFIG_NAME = "TP1_GRP3-frt-ip-config"
+NIC_NAME = "TP1_GRP3-frt-nic"
 
 # Obtain the management object for networks
 network_client = NetworkManagementClient(credential, subscription_id)
@@ -116,7 +119,130 @@ print(f"Provisioned network interface client {nic_result.name}")
 # Obtain the management object for virtual machines
 compute_client = ComputeManagementClient(credential, subscription_id)
 
-VM_NAME = "ExampleVM"
+VM_NAME = "VM-frontend-TP1"
+USERNAME = "azureuser"
+PASSWORD = "ChangePa$$w0rd24"
+
+print(f"Provisioning virtual machine {VM_NAME}; this operation might take a few minutes.")
+
+# Provision the VM specifying only minimal arguments, which defaults to an Ubuntu 18.04 VM
+# on a Standard DS1 v2 plan with a public IP address and a default virtual network/subnet.
+
+poller = compute_client.virtual_machines.begin_create_or_update(RESOURCE_GROUP_NAME, VM_NAME,
+    {
+        "location": LOCATION,
+        "storage_profile": {
+            "image_reference": {
+                "publisher": 'Canonical',
+                "offer": "UbuntuServer",
+                "sku": "16.04.0-LTS",
+                "version": "latest"
+            }
+        },
+        "hardware_profile": {
+            "vm_size": "Standard_DS1_v2"
+        },
+        "os_profile": {
+            "computer_name": VM_NAME,
+            "admin_username": USERNAME,
+            "admin_password": PASSWORD
+        },
+        "network_profile": {
+            "network_interfaces": [{
+                "id": nic_result.id,
+            }]
+        }        
+    }
+)
+
+vm_result = poller.result()
+
+print(f"Provisioned virtual machine {vm_result.name}")
+
+
+
+
+########################################       VM2 - backend   #########################
+
+# Step 2: provision a virtual network
+
+# A virtual machine requires a network interface client (NIC). A NIC requires
+# a virtual network and subnet along with an IP address. Therefore we must provision
+# these downstream components first, then provision the NIC, after which we
+# can provision the VM.
+
+# Network and IP address names
+VNET_NAME = "TP1_GRP3-bck"
+SUBNET_NAME = "TP1_GRP3-bck-subnet"
+IP_NAME = "TP1_GRP3-bck-ip"
+IP_CONFIG_NAME = "TP1_GRP3-bck-ip-config"
+NIC_NAME = "TP1_GRP3-bck-nic"
+
+# Obtain the management object for networks
+network_client = NetworkManagementClient(credential, subscription_id)
+
+# Provision the virtual network and wait for completion
+poller = network_client.virtual_networks.begin_create_or_update(RESOURCE_GROUP_NAME,
+    VNET_NAME,
+    {
+        "location": LOCATION,
+        "address_space": {
+            "address_prefixes": ["10.0.0.0/16"]
+        }
+    }
+)
+
+vnet_result = poller.result()
+
+print(f"Provisioned virtual network {vnet_result.name} with address prefixes {vnet_result.address_space.address_prefixes}")
+
+# Step 3: Provision the subnet and wait for completion
+poller = network_client.subnets.begin_create_or_update(RESOURCE_GROUP_NAME, 
+    VNET_NAME, SUBNET_NAME,
+    { "address_prefix": "10.0.0.0/24" }
+)
+subnet_result = poller.result()
+
+print(f"Provisioned virtual subnet {subnet_result.name} with address prefix {subnet_result.address_prefix}")
+
+# Step 4: Provision an IP address and wait for completion
+poller = network_client.public_ip_addresses.begin_create_or_update(RESOURCE_GROUP_NAME,
+    IP_NAME,
+    {
+        "location": LOCATION,
+        "sku": { "name": "Standard" },
+        "public_ip_allocation_method": "Static",
+        "public_ip_address_version" : "IPV4"
+    }
+)
+
+ip_address_result = poller.result()
+
+print(f"Provisioned public IP address {ip_address_result.name} with address {ip_address_result.ip_address}")
+
+# Step 5: Provision the network interface client
+poller = network_client.network_interfaces.begin_create_or_update(RESOURCE_GROUP_NAME,
+    NIC_NAME, 
+    {
+        "location": LOCATION,
+        "ip_configurations": [ {
+            "name": IP_CONFIG_NAME,
+            "subnet": { "id": subnet_result.id },
+            "public_ip_address": {"id": ip_address_result.id }
+        }]
+    }
+)
+
+nic_result = poller.result()
+
+print(f"Provisioned network interface client {nic_result.name}")
+
+# Step 6: Provision the virtual machine
+
+# Obtain the management object for virtual machines
+compute_client = ComputeManagementClient(credential, subscription_id)
+
+VM_NAME = "VM-back-TP1"
 USERNAME = "azureuser"
 PASSWORD = "ChangePa$$w0rd24"
 
