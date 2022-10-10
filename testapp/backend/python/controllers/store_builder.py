9da9931e0717f872ad949142c0f498eb068d6495
Julien Piguet <13250437+julienpiguet@users.noreply.github.com>
2022-10-10 17:43:44 +0200

    fix store builder

--- testapp/backend/python/controllers/store_builder.py
+++ testapp/backend/python/controllers/store_builder.py
@@ -3,10 +3,8 @@ from .local_store import LocalStore
 from .google_store import GoogleStore
 from .exoscale_store import ExoStore
 from .aws_store import AWSStore
-<<<<<<< HEAD
-=======
 from .azure_store import AzureStore
->>>>>>> a2aad9becf19835f4de021bdc0644be4398a8b85
+
 
 def get_store(name='local', arg = None):
     if name == 'google':
@@ -15,11 +13,8 @@ def get_store(name='local', arg = None):
         return ExoStore(arg) if arg != None else ExoStore()  
     elif name == 'aws':
         return AWSStore(arg) if arg != None else AWSStore()
-<<<<<<< HEAD
-=======
     elif name == 'azure':
         return AzureStore(arg) if arg != None else AzureStore()    
->>>>>>> a2aad9becf19835f4de021bdc0644be4398a8b85
     else:
         return LocalStore(arg) if arg != None else LocalStore()
     
